043984c3a77d981dfaf80d480ae3d62591cb05da
davi16 <dvidbeny@gmail.com>
2026-09-01 02:05:45 +0300

    Added trees, logs, grass and rocks to the terrain. added a small market around the settlement in the desert on the far side of the bridge. created realistic gravity for the player.

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
     public GameObject PlayerCamera; // public means that it must be connected in Unity
-    public float walkSpeed = 7.5f;   // חצי מהמהירות הקודמת להליכה רגילה
-    public float sprintSpeed = 15f;  // המהירות המקורית עבור ריצה
-    private float currentSpeed;      // משתנה עזר שיחזיק את המהירות הפעילה כרגע
+    public float walkSpeed = 7.5f;   // walking speed, currently set as half the sprinting speed
+    public float sprintSpeed = 15f;  // original sprinting speed
+    private float currentSpeed;      // helper variable to store the current speed
     float angular_speed = 3;
 
     CharacterController controller;
@@ -18,7 +18,7 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // בדיקה האם מקש השיפט השמאלי לחוץ
+        // setting the current speed based on whether LeftShift is pressed or not. If LeftShift is pressed, the player will sprint; otherwise, they will walk.
         if (Input.GetKey(KeyCode.LeftShift))
         {
             currentSpeed = sprintSpeed;
@@ -27,21 +27,31 @@ public class PlayerBehavior : MonoBehaviour
         {
             currentSpeed = walkSpeed;
         }
-        float dx, dz;
+
+        // set the speed to walkSpeed if LeftShift is not pressed, otherwise set it to sprintSpeed
+        float dx = Input.GetAxis("Horizontal") * currentSpeed;
+        float dz = Input.GetAxis("Vertical") * currentSpeed;
+
         float RotationAboutY = Input.GetAxis("Mouse X") * 3f;
         float RotationAboutX = -Input.GetAxis("Mouse Y") * 3f;
 
         PlayerCamera.transform.Rotate(RotationAboutX, 0, 0);
+        transform.Rotate(new Vector3(0, RotationAboutY, 0));
 
-        transform.Rotate(new Vector3(0, RotationAboutY,0));
+        Vector3 motion = new Vector3(dx, 0, dz); 
+        motion = transform.TransformDirection(motion); 
+        
+        // smart gravity: if the player is grounded, we apply a small downward force to keep them grounded; if they are in the air, we apply a realistic gravity force.
+        if (controller.isGrounded)
+        {
+            motion.y = -2f; // weak force to keep the player grounded when they are on the ground
+        }
+        else
+        {
+            motion.y = -9.81f; // realistic gravity force when the player is in the air
+        }
 
-        // שים לב: צריך להחליף את המילה 'speed' במילה 'currentSpeed' בשתי השורות הבאות:
-        dz = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
-        dx = Input.GetAxis("Horizontal") * currentSpeed * Time.deltaTime;
-
-        //        transform.Translate(new Vector3(dx, 0, dz));
-        Vector3 motion = new Vector3(dx, -1, dz); // -1 is gravity
-        motion = transform.TransformDirection(motion); // transforms motion to global coordinates
-        controller.Move(motion);//global coordinates
+        // the multiplication by Time.deltaTime is necessary to make the movement frame-rate independent, ensuring consistent movement speed regardless of the frame rate.
+        controller.Move(motion * Time.deltaTime);
     }
 }
